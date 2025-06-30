c3a0805f6f48fc2b18362a59daf4d48ee3498a14
Samman2 <shakyasamman92@gmail.com>
2025-06-30 17:19:13 +0545

    web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "db12c8384d2abb3f48b7af737b316ab0",
+const RESOURCES = {"flutter_bootstrap.js": "7b4ffe164e80a7f6ba4c87ffcf6db12d",
 "version.json": "1e6edca3e8bfdbac354ca41edb5c8e4d",
 "index.html": "29e1a782d449f6783a1d664d66d39eda",
 "/": "29e1a782d449f6783a1d664d66d39eda",
